b7831a248ff3efb69e4ba93609e6a84e510850f0
Helio Carlos <hcjr2000@gmail.com>
2021-09-07 20:36:20 -0300

    Handle add, remove and check task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,34 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function handleCreateNewId(): number {    
+    let n = Math.ceil(Math.random() * 1000000000)
+    return tasks.find(task => task.id === n) ? handleCreateNewId() : n
+  }
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    newTaskTitle !== '' && setTasks([...tasks, {
+      id: handleCreateNewId(),
+      title: newTaskTitle,
+      isComplete: false,
+    }])
+
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+    
+    setTasks(newTasks) 
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    let newTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(newTasks)
   }
 
   return (
